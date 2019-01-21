2f158c746f9987e99a2f5503b5bab4ce4c80f88c
Jonathan Mooney <moonejon@gmail.com>
2019-01-20 20:28:55 -0600

    working application

--- logic.js
+++ logic.js
@@ -1,4 +1,3 @@
-// Initialize Firebase
 var config = {
     apiKey: "AIzaSyDxpt5YbtSY_vWOQgP2Tez891JsATveFRA",
     authDomain: "train-scheduler-e6f77.firebaseapp.com",
@@ -7,15 +6,10 @@ var config = {
     storageBucket: "",
     messagingSenderId: "128442507709"
 };
+
 firebase.initializeApp(config);
 
 var database = firebase.database();
-
-database.ref().on(
-    'child_added', function (childSnapshot) {
-        console.log(childSnapshot)
-    }
-)
 
 var trainName = "";
 var destination = "";
@@ -24,61 +18,73 @@ var freq = 0;
 
 $(function onLoad() {
     document.getElementById("submitBtn").addEventListener("click", submitBtnClicked);
-    database.ref().on(
-        'child_added', function (childSnapshot) {
-            console.log(childSnapshot)
-        }
-    )
 });
 
 function submitBtnClicked() {
-    console.log("hey now");
     event.preventDefault();
     trainName = $("#trainName").val().trim();
     destination = $("#destination").val().trim();
     firstTrain = $("#firstTrain").val().trim();
     frequency = $("#freq").val().trim();
 
+    var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    var tRemainder = diffTime % frequency;
+
+    var minutesTillTrain = frequency - tRemainder;
+
+    var nextTrain = moment().add(minutesTillTrain, "minutes");
+
+
     var trainData = {
         trainName: trainName,
         destination: destination,
         firstTrain: firstTrain,
-        frequency: frequency
+        frequency: frequency,
+        nextTrain: moment(nextTrain).format("hh:mm"),
+        minutesAway: minutesTillTrain
     }
 
     database.ref().push(trainData);
-
-    // document.createElementById("div")
-
-
-
 };
-// database.ref().on(
-//     "child_added",
-//     function (childSnapshot) {
-//         var snapshotVal = childSnapshot.val();
-//         console.log("yada yada");
-//         var timesheetBody = document.getElementById("timesheetBody");
-//         var row = document.createElement("div")
-//         row.classList.add("row");
-//         timesheetBody.appendChild(row);
-//         var trainNameCol = document.createElement("div");
-//         trainNameCol.className = "col";
-//         trainNameCol.innerHTML = snapshotVal.trainName;
-//         var destinationCol = document.createElement("div");
-//         destination.className = "col";
-//         destinationCol.innerHTML = snapshotVal.destination;
-//         var firstTrainCol = document.createElement("div")
-//         firstTrainCol.className = "col";
-//         firstTrainCol.innerHTML = snapshotVal.firstTrain;
-//         var frequencyCol = document.createElement("div")
-//         frequencyCol.className = "col";
-//         frequencyCol.innerHTML = snapshotVal.frequency;
-//         row.appendChild(trainNameCol, destinationCol, firstTrainCol, frequencyCol);
 
-//         // tblRow.addClass("row")
-//         console.log(snapshotVal.trainName);
-//         console.log(snapshotVal.destination);
-//         console.log(snapshotVal.firstTrain);
-//         console.log(snapshotVal.frequency);
-//         });
+database.ref().on(
+    "child_added",
+    function (childSnapshot) {
+        var snapshotVal = childSnapshot.val();
+
+        var timesheetBody = document.getElementById("timesheetBody");
+        var row = document.createElement("div")
+        row.classList.add("row");
+        timesheetBody.appendChild(row);
+
+        var trainNameCol = document.createElement("div");
+        trainNameCol.className = "col";
+        trainNameCol.innerHTML = snapshotVal.trainName;
+
+        var destinationCol = document.createElement("div");
+        destinationCol.className = "col";
+        destinationCol.innerHTML = snapshotVal.destination;
+
+        var frequencyCol = document.createElement("div");
+        frequencyCol.className = "col";
+        frequencyCol.id = "frequency";
+        frequencyCol.innerHTML = snapshotVal.frequency;
+
+        var nextTrainCol = document.createElement("div");
+        nextTrainCol.className = "col";
+        nextTrainCol.innerHTML = snapshotVal.nextTrain;
+
+        var minutesAwayCol = document.createElement("div");
+        minutesAwayCol.classList.add("col")
+        minutesAwayCol.innerHTML = snapshotVal.minutesAway;
+
+        row.appendChild(trainNameCol);
+        row.appendChild(destinationCol);
+        row.appendChild(frequencyCol);
+        row.appendChild(nextTrainCol);
+        row.appendChild(minutesAwayCol);
+
+        });
